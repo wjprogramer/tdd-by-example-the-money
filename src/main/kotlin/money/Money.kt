fecffe4ecd336e5dafd8643d4ba9a3e5d7717018
jay_wu <jay_wu>
2021-03-25 20:35:07 +0800

    ch7: [fix] equals() not only compare with amount, but also type

--- src/main/kotlin/money/Money.kt
+++ src/main/kotlin/money/Money.kt
@@ -2,6 +2,8 @@ package money
 
 open class Money(open var amount: Int) {
     override fun equals(money: Any?): Boolean {
-        return money is Money && amount == money.amount
+        return money is Money
+                && this.javaClass == money.javaClass
+                && amount == money.amount
     }
 }
