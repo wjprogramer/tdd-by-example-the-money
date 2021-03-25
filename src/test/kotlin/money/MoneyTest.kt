8abd64e2232d2e63ace574a55fe2312d31b494f5
jay_wu <jay_wu>
2021-03-25 22:08:40 +0800

    ch8: [refactor] adjust test case from Dollar constructor to Money.dollar() factory method

--- src/test/kotlin/money/MoneyTest.kt
+++ src/test/kotlin/money/MoneyTest.kt
@@ -7,17 +7,17 @@ class MoneyTest {
     @Test
     fun testMultiplication() {
         val five: Money = Money.dollar(5)
-        assertEquals(Dollar(10), five.times(2))
-        assertEquals(Dollar(15), five.times(3))
+        assertEquals(Money.dollar(10), five.times(2))
+        assertEquals(Money.dollar(15), five.times(3))
     }
 
     @Test
     fun testEquality() {
-        assertTrue(Dollar(5) == Dollar(5))
-        assertFalse(Dollar(5) == Dollar (6))
+        assertTrue(Money.dollar(5) == Money.dollar(5))
+        assertFalse(Money.dollar(5) == Money.dollar (6))
         assertTrue(Franc(5) == Franc(5))
         assertFalse(Franc(5) == Franc(6))
-        assertFalse(Franc(5) == (Dollar(5) as Money))
+        assertFalse(Franc(5) == Money.dollar(5))
     }
 
     @Test
