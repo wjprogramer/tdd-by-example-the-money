4c7bdb36750d227982378472aa3dc48939e1222b
jay_wu <jay_wu>
2021-03-25 20:21:14 +0800

    ch6: [refactor] pull member up, Dollar.equals() move to super class Money class

--- src/main/kotlin/money/Dollar.kt
+++ src/main/kotlin/money/Dollar.kt
@@ -1,10 +1,6 @@
 package money
 
 class Dollar(amount: Int): Money(amount) {
-    override fun equals(money: Any?): Boolean {
-        return money is Money && amount == money.amount
-    }
-
     fun times(multiplier: Int): Dollar {
         return Dollar(amount * multiplier)
     }
--- src/main/kotlin/money/Money.kt
+++ src/main/kotlin/money/Money.kt
@@ -1,5 +1,7 @@
 package money
 
 open class Money(open var amount: Int) {
-
+    override fun equals(money: Any?): Boolean {
+        return money is Money && amount == money.amount
+    }
 }
