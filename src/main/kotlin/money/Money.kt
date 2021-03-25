a1475ed34be7540e897430655ca0279d608ad3fb
jay_wu <jay_wu>
2021-03-25 22:03:18 +0800

    ch8: [feat] use factory method to return Dollar instance

--- src/main/kotlin/money/Money.kt
+++ src/main/kotlin/money/Money.kt
@@ -1,6 +1,10 @@
 package money
 
 open class Money(open var amount: Int) {
+    companion object {
+        fun dollar(amount: Int): Dollar = Dollar(amount)
+    }
+
     override fun equals(money: Any?): Boolean {
         return money is Money
                 && this.javaClass == money.javaClass
