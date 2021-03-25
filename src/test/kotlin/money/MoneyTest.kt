d068625f68c953b8d653d0753fc5e7001a7e7af4
jay_wu <jay_wu>
2021-03-25 20:33:45 +0800

    ch7: [experiment] equality compare with Franc and Dollar?

--- src/test/kotlin/money/MoneyTest.kt
+++ src/test/kotlin/money/MoneyTest.kt
@@ -15,8 +15,9 @@ class MoneyTest {
     fun testEquality() {
         assertTrue(Dollar(5) == Dollar(5))
         assertFalse(Dollar(5) == Dollar (6))
-        assertTrue(Franc(5) == Franc(5));
-        assertFalse(Franc(5) == Franc(6));
+        assertTrue(Franc(5) == Franc(5))
+        assertFalse(Franc(5) == Franc(6))
+        assertFalse(Franc(5) == (Dollar(5) as Money))
     }
 
     @Test
