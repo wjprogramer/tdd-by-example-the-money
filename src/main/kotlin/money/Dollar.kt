a7125175cbc7ea707e2bfc72dba03023034a772e
jay_wu <jay_wu>
2021-03-25 20:20:20 +0800

    ch6: [refactor] rename variable "dollar" to "money" to match its type

--- src/main/kotlin/money/Dollar.kt
+++ src/main/kotlin/money/Dollar.kt
@@ -1,8 +1,8 @@
 package money
 
 class Dollar(amount: Int): Money(amount) {
-    override fun equals(obj: Any?): Boolean {
-        return obj is Money && amount == obj.amount
+    override fun equals(money: Any?): Boolean {
+        return money is Money && amount == money.amount
     }
 
     fun times(multiplier: Int): Dollar {
