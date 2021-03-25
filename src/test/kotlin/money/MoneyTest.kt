f93edcf11dce0a495f9caa58c875792ef45a8339
jay_wu <jay_wu>
2021-03-25 19:55:48 +0800

    ch2: [test] define how we want to use times(), it should return another Dollar object

--- src/test/kotlin/money/MoneyTest.kt
+++ src/test/kotlin/money/MoneyTest.kt
@@ -7,9 +7,9 @@ class MoneyTest {
     @Test
     fun testMultiplication() {
         val five = Dollar(5)
-        five.times(2)
-        assertEquals(10, five.amount)
-        five.times(3)
-        assertEquals(15, five.amount)
+        var product = five.times(2)
+        assertEquals(10, product.amount)
+        product = five.times(3)
+        assertEquals(15, product.amount)
     }
 }
