2472f6acf8f56863a3461572fdc5ff85160a65e2
jay_wu <jay_wu>
2021-03-25 21:59:38 +0800

    ch8: [refactor] make Dollar's times() and Franc's times() return the same type Money, to show them duplicated in abstract level

--- src/main/kotlin/money/Franc.kt
+++ src/main/kotlin/money/Franc.kt
@@ -1,7 +1,7 @@
 package money
 
 class Franc(amount: Int): Money(amount) {
-    operator fun times(multiplier: Int): Franc {
+    operator fun times(multiplier: Int): Money {
         return Franc(amount * multiplier)
     }
 }
