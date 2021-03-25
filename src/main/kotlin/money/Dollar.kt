f1b37c9077b01a618fe68fcb798bd337d590d4dd
jay_wu <jay_wu>
2021-03-25 22:15:59 +0800

    ch9: [refactor] extract field "currency" from hard-code constant "USD" and initialize in constructor in Dollar class

--- src/main/kotlin/money/Dollar.kt
+++ src/main/kotlin/money/Dollar.kt
@@ -1,9 +1,11 @@
 package money
 
 class Dollar(amount: Int): Money(amount) {
+    private val currency = "USD"
+
     override fun times(multiplier: Int): Money {
         return Dollar(amount * multiplier)
     }
 
-    override fun currency() = "USD"
+    override fun currency() = currency
 }
