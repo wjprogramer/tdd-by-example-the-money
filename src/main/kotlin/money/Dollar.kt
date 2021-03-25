32250fbce1a8c8340e6922beba2ea6280a7222de
jay_wu <jay_wu>
2021-03-25 19:59:25 +0800

    ch3: [fake it] override Dollar.equals() and hard-code return true

--- src/main/kotlin/money/Dollar.kt
+++ src/main/kotlin/money/Dollar.kt
@@ -1,6 +1,10 @@
 package money
 
 class Dollar(var amount: Int) {
+    override fun equals(obj: Any?): Boolean {
+        return true
+    }
+
     fun times(multiplier: Int): Dollar {
         return Dollar(amount * multiplier)
     }
