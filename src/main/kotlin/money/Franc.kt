3419253b7419c36d886ed141aea96014baff2435
jay_wu <jay_wu>
2021-03-25 20:25:49 +0800

    ch6: [refactor] remove Franc's equals() because of duplication with Super class Money's equals()

--- src/main/kotlin/money/Franc.kt
+++ src/main/kotlin/money/Franc.kt
@@ -1,11 +1,6 @@
 package money
 
 class Franc(amount: Int): Money(amount) {
-    override fun equals(money: Any?): Boolean {
-        val franc: Money? = money as Money?
-        return amount == franc!!.amount
-    }
-
     operator fun times(multiplier: Int): Franc {
         return Franc(amount * multiplier)
     }
