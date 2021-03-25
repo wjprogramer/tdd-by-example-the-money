9baebde4cab28a49a2b36135820ffe23fa6ec537
jay_wu <jay_wu>
2021-03-25 22:13:37 +0800

    ch9: [feat] Franc and Dollar class implement abstract currency() to return its currency content

--- src/main/kotlin/money/Dollar.kt
+++ src/main/kotlin/money/Dollar.kt
@@ -4,4 +4,6 @@ class Dollar(amount: Int): Money(amount) {
     override fun times(multiplier: Int): Money {
         return Dollar(amount * multiplier)
     }
+
+    override fun currency() = "USD"
 }
--- src/main/kotlin/money/Franc.kt
+++ src/main/kotlin/money/Franc.kt
@@ -4,4 +4,6 @@ class Franc(amount: Int): Money(amount) {
     override fun times(multiplier: Int): Money {
         return Franc(amount * multiplier)
     }
+
+    override fun currency(): String = "CHF"
 }
