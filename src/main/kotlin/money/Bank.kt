3fb541d0e072fc39937a5cfd6fba1904687759ae
jay_wu <jay_wu>
2021-03-26 22:25:45 +0800

    ch13: [refactor] move reduce() algorithm to Sum.reduce() to make field can be non-public

--- src/main/kotlin/money/Bank.kt
+++ src/main/kotlin/money/Bank.kt
@@ -3,7 +3,6 @@ package money
 class Bank {
     fun reduce(source: Expression, to: String): Money {
         val sum = source as Sum
-        val amount = sum.augend.amount + sum.addend.amount
-        return Money(amount, to)
+        return sum.reduce(to)
     }
 }
--- src/main/kotlin/money/Sum.kt
+++ src/main/kotlin/money/Sum.kt
@@ -1,4 +1,8 @@
 package money
 
 class Sum(val augend: Money, val addend: Money): Expression {
+    fun reduce(to: String): Money {
+        val amount = augend.amount + addend.amount
+        return Money(amount, to)
+    }
 }
