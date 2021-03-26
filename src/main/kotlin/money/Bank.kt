e9aad3476df96fb52d85d60b6d4a9f0398ff9c51
jay_wu <jay_wu>
2021-03-26 22:13:08 +0800

    ch12: [fake it] hard-code return Money.dollar(10) in Bank.reduce()

--- src/main/kotlin/money/Bank.kt
+++ src/main/kotlin/money/Bank.kt
@@ -1,7 +1,7 @@
 package money
 
 class Bank {
-    fun reduce(source: Expression, to: String): Money? {
-        return null
+    fun reduce(source: Expression, to: String): Money {
+        return Money.dollar(10)
     }
 }
