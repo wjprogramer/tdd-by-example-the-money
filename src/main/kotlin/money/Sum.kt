4a8504f83716eb82e461e14433ca039e3887ce1d
jay_wu <jay_wu>
2021-03-27 00:33:09 +0800

    ch15: [refactor] declare fields "augend" and "addend" as Expression, instead of Money

--- src/main/kotlin/money/Sum.kt
+++ src/main/kotlin/money/Sum.kt
@@ -1,6 +1,6 @@
 package money
 
-class Sum(val augend: Money, val addend: Money): Expression {
+class Sum(val augend: Expression, val addend: Expression): Expression {
     override fun reduce(bank: Bank, to: String): Money {
         val amount = (augend.reduce(bank, to).amount
                 + addend.reduce(bank, to).amount)
