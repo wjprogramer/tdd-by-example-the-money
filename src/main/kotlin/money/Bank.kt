85724975547e889856b300d228059ca4bea1da05
jay_wu <jay_wu>
2021-03-26 23:56:40 +0800

    ch13: [refactor] remove useless if condition by polymorphism as Expression

--- src/main/kotlin/money/Bank.kt
+++ src/main/kotlin/money/Bank.kt
@@ -2,9 +2,6 @@ package money
 
 class Bank {
     fun reduce(source: Expression, to: String): Money {
-        if (source is Money)
-            return source.reduce(to) as Money
-        val sum = source as Sum
-        return sum.reduce(to)
+        return source.reduce(to)
     }
 }
