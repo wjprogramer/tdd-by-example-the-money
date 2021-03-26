500e2e4011a985b7462f374ac6a322a2f21d2c7d
jay_wu <jay_wu>
2021-03-27 00:16:03 +0800

    ch14: [feat] override Pair.equals() and hashCode()

--- src/main/kotlin/money/Bank.kt
+++ src/main/kotlin/money/Bank.kt
@@ -14,5 +14,14 @@ class Bank {
     }
 
 
-    private class Pair(private val from: String, private val to: String)
+    private class Pair(private val from: String, private val to: String) {
+        override fun equals(other: Any?): Boolean {
+            val pair: Pair = other as Pair
+            return from == pair.from && to == pair.to
+        }
+
+        override fun hashCode(): Int {
+            return 0
+        }
+    }
 }
