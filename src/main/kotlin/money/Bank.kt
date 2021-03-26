8347e84c02efb8621892c3b65d4a2fb980feaf61
jay_wu <jay_wu>
2021-03-27 00:10:54 +0800

    ch14: [experiment] create rate() of Bank class, and copying the fake implementation from Money.reduce()

--- src/main/kotlin/money/Bank.kt
+++ src/main/kotlin/money/Bank.kt
@@ -4,4 +4,12 @@ class Bank {
     fun reduce(source: Expression, to: String): Money {
         return source.reduce(this, to)
     }
+
+    fun addRate(source: String, to: String, rate: Int) {
+
+    }
+
+    fun rate(from: String, to: String): Int {
+        return if (from == "CHF" && to == "USD") 2 else 1
+    }
 }
