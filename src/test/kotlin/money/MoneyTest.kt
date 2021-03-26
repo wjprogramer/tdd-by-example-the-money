c39e6bb54240618296f8f027391f2ae1bb9664c6
jay_wu <jay_wu>
2021-03-26 22:07:18 +0800

    ch12: [experiment] we need a bank

--- src/test/kotlin/money/MoneyTest.kt
+++ src/test/kotlin/money/MoneyTest.kt
@@ -36,6 +36,7 @@ class MoneyTest {
         val sum: Money = Money.dollar(5) + Money.dollar(5)
         assertEquals(Money.dollar(10), sum)
         // opposite down, starting from assertion and backward to find(think) where it came from
+        val bank = Bank()
         val reduced: Money = bank.reduce(sum, "USD")
         assertEquals(Money.dollar(10), reduced)
     }
