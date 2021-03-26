b71789a6e9a8533400d42579a5b98f021c378ccd
jay_wu <jay_wu>
2021-03-26 22:26:30 +0800

    ch13: [test] add test when bank.reduce() pass a money as Expression parameter. And hard-code if condition only for Money type

--- src/main/kotlin/money/Bank.kt
+++ src/main/kotlin/money/Bank.kt
@@ -1,7 +1,13 @@
 package money
 
+import jdk.nashorn.internal.objects.NativeRegExp.source
+
+
+
+
 class Bank {
     fun reduce(source: Expression, to: String): Money {
+        if (source is Money) return source
         val sum = source as Sum
         return sum.reduce(to)
     }
--- src/test/kotlin/money/MoneyTest.kt
+++ src/test/kotlin/money/MoneyTest.kt
@@ -56,4 +56,11 @@ class MoneyTest {
         val result = bank.reduce(sum, "USD")
         assertEquals(Money.dollar(7), result)
     }
+
+    @Test
+    fun testReduceMoney() {
+        val bank = Bank()
+        val result = bank.reduce(Money.dollar(1), "USD")
+        assertEquals(Money.dollar(1), result)
+    }
 }
