1e38f8aa01c2555899858cbd7701987f7468800e
jay_wu <jay_wu>
2021-03-26 22:08:36 +0800

    ch12: [experiment] sum should be an expression that came from plus() operation

--- src/test/kotlin/money/MoneyTest.kt
+++ src/test/kotlin/money/MoneyTest.kt
@@ -33,9 +33,10 @@ class MoneyTest {
 
     @Test
     fun testSimpleAddition() {
-        val sum: Money = Money.dollar(5) + Money.dollar(5)
-        assertEquals(Money.dollar(10), sum)
+//        val sum: Money = Money.dollar(5) + Money.dollar(5)
+//        assertEquals(Money.dollar(10), sum)
         // opposite down, starting from assertion and backward to find(think) where it came from
+        val sum: Expression = five.plus(five)
         val bank = Bank()
         val reduced: Money = bank.reduce(sum, "USD")
         assertEquals(Money.dollar(10), reduced)
