bf1e70d590504ea4cef164ac9879bcc17a4f2585
jay_wu <jay_wu>
2021-03-26 23:59:31 +0800

    ch14: [test] add test reduce money when different currency

--- src/test/kotlin/money/MoneyTest.kt
+++ src/test/kotlin/money/MoneyTest.kt
@@ -63,4 +63,12 @@ class MoneyTest {
         val result = bank.reduce(Money.dollar(1), "USD")
         assertEquals(Money.dollar(1), result)
     }
+
+    @Test
+    fun testReduceMoneyDifferentCurrency() {
+        val bank = Bank()
+        bank.addRate("CHF", "USD", 2)
+        val result = bank.reduce(Money.franc(2), "USD")
+        assertEquals(Money.dollar(1), result)
+    }
 }
