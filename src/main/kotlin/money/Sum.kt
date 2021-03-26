25c3ac2e5e01fa5006e19a5ebbfb3b742e80f3bc
jay_wu <jay_wu>
2021-03-27 00:32:47 +0800

    ch15: [feat] complete Sum.reduce() by Money.reduce(bank,to)

--- src/main/kotlin/money/Sum.kt
+++ src/main/kotlin/money/Sum.kt
@@ -2,7 +2,8 @@ package money
 
 class Sum(val augend: Money, val addend: Money): Expression {
     override fun reduce(bank: Bank, to: String): Money {
-        val amount = augend.amount + addend.amount
+        val amount = (augend.reduce(bank, to).amount
+                + addend.reduce(bank, to).amount)
         return Money(amount, to)
     }
 }
